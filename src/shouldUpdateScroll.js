f3af8fef5acbe873c3a5c682a871a2ec7421ef5d
Danry Ague <74456102+decanTyme@users.noreply.github.com>
2022-04-09 00:44:14 +0800

    refactor: Format with Prettier

--- src/shouldUpdateScroll.js
+++ src/shouldUpdateScroll.js
@@ -1,8 +1,6 @@
 import _ from 'lodash'
 
-const shouldUpdateScroll = ({
-  routerProps: { location }
-}) => {
+const shouldUpdateScroll = ({ routerProps: { location } }) => {
   const isModal = _.get(location, 'state.modal')
   const preventUpdateScroll = _.get(location, 'state.noScroll')
 
