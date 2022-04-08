f5bb5949b12f201e1cefb6c8236ec0f7b1ffff41
Danry Ague <74456102+decanTyme@users.noreply.github.com>
2022-04-09 00:00:25 +0800

    refactor: Re-structure files and dirs

---
+++ index.js
@@ -0,0 +1,7 @@
+const { default: ModalRoutingContext } = require("./dist/ModalRoutingContext");
+const { default: ModalRoutingLink } = require("./dist/ModalRoutingLink");
+
+module.exports = {
+  Link: ModalRoutingLink,
+  ModalRoutingContext,
+};
